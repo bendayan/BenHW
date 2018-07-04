8bef9b2b5f141de260ae5892c4536e982a623a63
bendayan <benchu80@gmail.com>
2018-07-04 18:27:32 +0300

    HW9js1

---
+++ HW9JS/javaScriptFile.js
@@ -0,0 +1,32 @@
+{
+
+
+
+
+
+
+var randnum= 10;
+
+
+document.getElementById("example").innerHTML= "Number is :" +randnum+ " ";
+
+randnum =(10*10) /2 ;
+document.getElementById("td2").innerHTML="area is:"+randnum+"  ";
+
+randnum*=3;
+document.getElementById("td3").innerHTML="perimeter is:"+randnum+"  ";
+
+randnum=(10*10)*3.142;
+document.getElementById("td4").innerHTML="perimeter is:"+randnum+"  ";
+
+randnum=(10*2)*3.142;
+document.getElementById("td5").innerHTML="perimeter is:"+randnum+"  ";
+
+randnum=Math.pow(10,2);
+document.getElementById("td6").innerHTML="perimeter is:"+randnum+"  ";
+randnum=10;
+randnum*=4;
+document.getElementById("td7").innerHTML="perimeter is:"+randnum+"  ";
+
+}
+
---
+++ שיעור 9 גאווה סקריפט/javaScriptFile.js
@@ -0,0 +1,65 @@
+{
+
+
+  document.write("write hello");
+  //document.getElementById("example").innerHTML="  hello  by id ";
+
+  document.getElementById("example1").innerHTML=" heyy ";
+
+
+  var name= "ben"; 
+  var age=16;
+
+  console.log(name +" " + age+"");
+
+  if(age >18)
+  console.log(name +" no legal");
+  else
+  console.log (name + " legal");
+
+var age = 5 -4.5;
+console.log(age);
+
+age /=2;
+console.log(age);
+
+var age1=20;
+
+if(age1 %2==0)
+console.log("even");
+else
+console.log("odd");
+
+
+var num1=1 ,num2=2; num3=3,num4=4;
+var result;
+var randnum= "Number=10:";
+var lastname ="dayan";
+
+if(num1*num2*num3*num4 > 1000000)
+alert(":)");
+else
+alert(":(");
+
+document.getElementById("example").innerHTML=randnum+ " "   ;
+
+if(num4-num1 % 2==0)
+alert("even");
+else
+alert("odd");
+num3++;
+alert(num3);
+
+
+
+
+}
+
+function myFunction() {
+  var num9=0;
+  for(var i=0; i<10; i++){
+  num9+=i;
+  }
+  document.write(num9);
+
+}
