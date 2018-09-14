838ba223e388cf212f9c71e2cffed8584279c972
bendayan <benchu80@gmail.com>
2018-09-14 18:05:38 +0300

    message111

--- HW16/javaScriptFile.js
+++ HW16/javaScriptFile.js
@@ -1,105 +1,5 @@
 {
 
-/*
-    function func1 (){
-   var arr=[];
-   var sum=0;
-arr[0] = document.getElementById("test").value;
-arr[1] = document.getElementById("test1").value;
-arr[2] = document.getElementById("test2").value;
-
-sum=(parseInt(arr[0])+parseInt(arr[1])+parseInt(arr[2]));
-document.getElementById("print").innerHTML=arr;
-//document.getElementById("print2").innerHTML=sum;
-document.getElementById("print2").innerHTML=sum/arr.length;
-
-//alert(arr);
-}
-*/
-
-
-// function func2 (){
-//     var numInput= document.getElementById("idnum").value;
-
-// var arr1=[1,2,2,4,5];
-
-// var count=0;
-// var i=0;
-
-// while (i<arr1.length){
-
-//     if(arr1[i]==numInput){
-//     count++;
-    
-        
-//     }
-//     i++;
-// }
- 
-
-//     alert(count);
-    
-
-
-// }
-
-/*
-//JQuery
-$(document).ready(function(){
-  $("#input1").mouseenter(function(){
-      $("#input1").css("background-color","blue");
-});
-$("#input1").mouseleave(function(){
-  $("#input1").css("background-color","yellow");
-
-});
-$("#input1").mousedown(function(){
-  $("#input1").css("background-color","red");
-});
-
-});
-*/
-
-
-
-/*
-function func2() {
-  document.getElementById("p1").style.color="red";
-  document.getElementById("p1").innerHTML="hello";
-
-}
-function func1() {
-    
-    document.getElementById("p1").style.color="blue";
-  
-  }
-
-
-
-*/
-
-/*
-var arr =[];
- arr=document.getElementById("inputarray").value;
-var i=0;
-for(i=0; i<arr.length; i++)
-if(arr[i]>arr[i+1])
-alert(arr[i]);
-
-}
-*/
-
-
-
-
-$(document).ready(function(){
-  $("#b1").click(function(){
-    $.ajax({
-      url:"txt1.csv",
-      dataType:"text",
-      success:function(data)
-      {
-        var celdata = data.split(/\r?\n|\r/);
         //var MYworld ="";
         //var flag=true;
 
@@ -108,83 +8,84 @@ $(document).ready(function(){
       // alert(celdata[0]);
      //  alert(celdata[0][0]);
     
-       var fname ="";
-       var lname ="";
-       var phonenumber ="";
-       var email1 ="";
-   
-       var i,j;
-       var count=0;
-       for(i=1; i<celdata.lenght; i++){
-         for(j=0; j<celdata[i].lenght; j++){
-          if(cellData[i][j]==','||cellData[i].length-1==j)
-           count++;
-         }
-         if(celdata[i][j] !=  ',')
-        if(count==0)
-           fname+=celdata[i][j];
-             if(count==1)
-             lname+=celdata[i][j];
-                if(count==2)
-                phonenumber+=celdata[i][j];
-                   (count==3)
-                      email1+=celdata[i][j];
+    
+  
+       function findRowName(db,name){
+      
+        let i=1;
+        let j=0;
+        var tempName="";
+        for(;i<db.length;i++){
+    for(j=0;db[i][j]!=',';j++)
+             tempName+=db[i][j];
+        if(tempName==name)
+          return i;//return the db row its found
 
-       }
-      alert(count);
+       tempName="";
+        }
+      
+        return -1;//in name not found in db
+           
       }
-
-    });
- 
-
-  });
-});
-
-
-
-
+              
+         
 function selectedchoise(){
 
   var selectdrop = document.getElementById("selected1");
   var selectvalue1= selectdrop.options[selectdrop.selectedIndex].value;
 
- 
-   document.getElementById("idinputname").innerHTML=fname;
+$(document).ready(function(){
+
+  $.ajax({
+    url:"txt1.csv",
+    dataType:"text",
+    success:function(data)
+    {
+      var celdata = data.split(/\r?\n|\r/);
+      var row=findRowName(celdata,selectvalue1);
+      printinfo(row,celdata);
+    }
+  });
+});
+function printinfo(row,cellData){
+     var firstName ="";
+       var lastName ="";
+       var phone ="";
+       var email ="";
+   
+       var i,j;
+       var cnt=0;
+       for(i=row,j=0; j<cellData[row].length; j++){
+        
+        if(cellData[i][j]==','){
+          cnt++;
+          continue;
+          }
+     if(cnt==0)
+     firstName+=cellData[i][j];
+     else if(cnt==1)
+     lastName+=cellData[i][j];
+    else if(cnt==2)
+     phone+=cellData[i][j];
+    else  
+      email+=cellData[i][j];
+   
+
+       }
+       alert(phone);
+      }
 
 
-   document.getElementById("idinputlastname").innerHTML=lname;
+//    document.getElementById("idinputname").innerHTML=fname;
+   
+
+//    document.getElementById("idinputlastname").innerHTML=lname;
   
 
-document.getElementById("idinputnum").innerHTML=phonenumber;
+// document.getElementById("idinputnum").innerHTML=phonenumber;
 
- document.getElementById("idinputemail").innerHTML=email1;
+//  document.getElementById("idinputemail").innerHTML=email1;
 }
-
-
-
-
-
-
-
-
-
-
-
-// function func1 (){
-//     var arr=[];
-//   //   var sum=0;
-//  arr.push ( document.getElementById("test").value);
-//  arr.push ( document.getElementById("test1").value);
-//  arr.push (document.getElementById("test2").value);
- 
-//  //sum=(parseInt(arr[0])+parseInt(arr[1])+parseInt(arr[2]));
-//  //document.getElementById("print").innerHTML=arr;
-//  document.getElementById("print").innerHTML=arr.toString();
-// }
-
-
-
-
 
 
 
